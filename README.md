8b2a038adc5474ba0df1a774421a6b97e4a1524f
Rob Phoenix <rob.phoenix@bt.com>
2015-06-29 15:31:12 +0100

    updated README to include Database info, removed virtualenv & virtualenvwrapper from pip requirements file, updated setup files

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-# Django development Vagrantfile
+# Vagrantfile for Django development
 
-A Vagrantfile that brings up 2 VMs, one for Django, the other with a PostgreSQL database.
+> A Vagrantfile that brings up 2 VMs, one for Django, the other with a PostgreSQL database.
 
-To access Django development server:
-- run `python manage.py runserver 0.0.0.0:8000`
-- access on `http://localhost:8001/`
+## To access Django development server on guest, from Host:
+- run `python manage.py runserver 0.0.0.0:8000` on guest.
+- access on `http://localhost:8001/` on host.
+
+## Database settings:
+- in `settings.py`:
+```
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lld_db',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
+```
--- provision/djangonode-setup.sh
+++ provision/djangonode-setup.sh
@@ -10,18 +10,13 @@ echo " "
 sudo apt-get -y update > /dev/null
 
 echo " "
-echo "---{ Installing pip & virtualenv..."
+echo "---{ Installing pip & associated requirements..."
 echo " "
 sudo apt-get -y install python-pip python-dev build-essential > /dev/null
 sudo pip install --upgrade pip > /dev/null
-sudo pip install --upgrade virtualenv virtualenvwrapper > /dev/null
+# libpq-dev is a dependency of psycopg2
 sudo apt-get -y install libpq-dev > /dev/null
-# set the location where the virtual environments should live, and the location of the script installed with virtualenvwrapper
-sudo echo " " >> /home/vagrant/.bashrc > /dev/null
-sudo echo "export WORKON_HOME=/home/vagrant/.virtualenvs" >> /home/vagrant/.bashrc > /dev/null
-sudo echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/vagrant/.bashrc > /dev/null
-# reload startup file
-source /home/vagrant/.bashrc > /dev/null
+sudo pip install -r /home/vagrant/shared/requirements.txt > /dev/null
 
 echo " "
 echo "---{ VM provisioned."
